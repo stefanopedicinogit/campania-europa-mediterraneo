f0f57baa0c2c6f4d0e0d524e9014086394b3d290
Stefano Pedicino <83118026+stefanopedicinogit@users.noreply.github.com>
2025-07-24 02:04:20 +0200

    fixed bug gallery incontri estate

--- js/incontriestate.js
+++ js/incontriestate.js
@@ -58,7 +58,7 @@
 
 
 function fetchImages() {
-    const folderPath = '/assets/manifesti/manifesti-incontri-estate/';
+    const folderPath = '/assets/manifesti/manifestiincontriestate/';
     console.log('folderPath', folderPath);
     const imageExtensions = ['jpg', 'jpeg', 'png', 'gif'];
     const sliderContainer = document.querySelector('.slider-container');
